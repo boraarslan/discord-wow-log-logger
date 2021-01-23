3c18df7c586a3b4d6f1e2c68a81c7d849e0b669d
Bora Arslan <bora-arslan@outlook.com>
2021-01-23 11:03:39 +0300

    query edit and bug fix

--- logsApi/reportData.js
+++ logsApi/reportData.js
@@ -9,14 +9,17 @@ module.exports = async function (token, code) {
 			Authorization: "Bearer " + token,
 		},
 		data: {
-			query: `query reportData {\n  reportData{\n    report(\n      code: ${code}\n    ) {\n      graph(dataType:DamageDone, startTime:0 , endTime:999999999)\n      rankings(compare:Parses)\n    }\n  }\n}","operationName":"reportData"}`,
+			query: `query reportData {\n  reportData{\n    report(\n      code: "${code}"\n    ) {\n      graph(dataType:DamageDone, startTime:0 , endTime:999999999)\n      rankings(compare:Parses)\n    }\n  }\n}`,
 			operationName: "reportData",
 		},
 	};
-	return axios.request(options).then(function (response) {
-		return response.data.data;
-	}).catch(function (error) {
-		console.error(error);
-		throw error;
-	});
+	return axios
+		.request(options)
+		.then(function (response) {
+			return response.data.data.reportData;
+		})
+		.catch(function (error) {
+			console.error(error);
+			throw error;
+		});
 };
